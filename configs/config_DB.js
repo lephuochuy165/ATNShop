52c8a90f6dd983c416140697af6f3cf118b5da7a
lephuochuy165 <huylpgcs18720@fpt.edu.vn>
2024-06-10 23:21:36 +0700

    first commit

---
+++ configs/config_DB.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//     clouddb: {
+//         urldb : 'mongodb+srv://trungshin_6969:QK3xrnkUn8MUUBUw@cluster0.krj6n.mongodb.net/shopsmall?retryWrites=true&w=majority',
+//         dbname : "shopsmall",
+//         dbusername : "",
+//         dbpassword : "",
+//     }
+
+// };
+
+module.exports = {
+    clouddb: {
+        urldb : 'mongodb+srv://huylpgcs18720:mi7NDMHp7bablxvN@cluster0.t4lrva0.mongodb.net/atnshop?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "",
+        dbpassword : "mi7NDMHp7bablxvN",
+    }
+
+};
+
